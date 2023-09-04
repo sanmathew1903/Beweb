62541862f077d6126fdf35e1ee3002331e24bfb5
san <sanmathew2002@gmail.com>
2023-09-04 19:39:30 +0530

    created login and signin page

--- blood_bank/home/urls.py
+++ blood_bank/home/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns={
-    path('',views.index,name='index'),
+    
+    path('',views.loginpage,name='loginpage'),
+    path('index',views.index,name='index'),
     path('add',views.add,name='add'),
-    path('display',views.display,name='display')
-    #path('display',views.display,name='display')
-
+    path('display',views.display,name='display'),
+    path('signin',views.signin,name='signin'),
+    path('login',views.login,name='login'),
+    path('login_info',views.login_info,name='login_info'),
+    path('signin_info',views.signin_info,name='signin_info')
+    
+    
 }
--- blood_bank/home/views.py
+++ blood_bank/home/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 userlist=[]
+users=[]
 # Create your views here.
+
+def signin(request):
+    return render(request,'signin.html')
+
+def signin_info(request):
+    username=request.POST['username']
+    password=request.POST['password']
+    dict={username:password}
+    users.append(dict)
+    return render(request,'loginpage.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def login_info(request):
+    username=request.POST['username']
+    password=request.POST['password']
+    for i in users:
+        for j in i:
+            if j==username and i[j]==password:
+                return render(request,'index.html')
+    return HttpResponse("<html><body><h1>NO USER FOUND</h1></body></html>")
+
+
+
+def loginpage(request):
+    return render(request,'loginpage.html')
+
 def index(request):
     return render(request,'index.html')
 
